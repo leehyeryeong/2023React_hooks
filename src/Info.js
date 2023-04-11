80147088e2bf8a29cc8cc514411f4fff30ed6d47
leehyeryeong <w2108@e-mirim.hs.kr>
2023-04-11 15:08:34 +0900

    특정값이 업데이트 될 때만 실행

--- src/Info.js
+++ src/Info.js
@@ -4,11 +4,11 @@ const Info = () => {
   const[name, setName] = useState('');
   const [nickname, setNickname] = useState('');
 
-  //컴포넌트가 처음 마운트 될 때 2번 렌더링(실행)이 발생한다.
-  //state(name 또는 nickname) 값이 변경될 때마다 렌더링이 발생한다.
+  //useEffect의 두번째 인자값을 [name]으로 표시하면
+  //state의 name 값이 변경 될 때마다 실행한다.
   useEffect(() => {
-    console.log("마운트 될 때만 실행됩니다.");
-  }, []);
+    console.log(name);
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
